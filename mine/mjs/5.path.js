708a277cd6b5001367124ea8ec1d0e341040035f
Alex <alejandro.vazquez@watlow.com>
2025-11-12 08:59:00 -0600

    Merge

--- mine/mjs/5.path.js
+++ mine/mjs/5.path.js
@@ -1,6 +1,6 @@
 const path = require('node:path');
 //unir rutas con path.join
-//barra separadora de carpetas
+//barra separadora de carpetas segun so
 console.log(path.sep);
 //te muestra los slahs dependiendo el sistema operativo
 const filePath = path.join('content', 'subfolder', 'test.txt');
